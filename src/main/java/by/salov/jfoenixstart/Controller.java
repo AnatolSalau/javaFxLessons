484c981212e06778eff93e0b47c1ce04b2c5c152
AnatolSalau <salovanatoly5@mail.ru>
2022-08-15 16:25:33 +0300

    added lesson svgload

--- src/main/java/by/salov/jfoenixstart/Controller.java
+++ src/main/java/by/salov/jfoenixstart/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.Region;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,7 +31,6 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         TreeTableColumn column1 = new TreeTableColumn<>("Фамилия");
-        //column1.setStyle();
         TreeTableColumn column2 = new TreeTableColumn<>("Имя");
         TreeTableColumn column3 = new TreeTableColumn<>("Отчество");
         tableview.getColumns().addAll(column1,column2,column3);
