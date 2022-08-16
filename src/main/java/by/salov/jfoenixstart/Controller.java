fa398309cdc2b91f272f35b310141540d55e0c5b
AnatolSalau <salovanatoly5@mail.ru>
2022-08-16 22:08:53 +0300

    added lesson svgload

--- src/main/java/by/salov/jfoenixstart/Controller.java
+++ src/main/java/by/salov/jfoenixstart/Controller.java
@@ -2,11 +2,17 @@ package by.salov.jfoenixstart;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.Region;
 
 import java.net.URL;
@@ -27,7 +33,7 @@ public class Controller implements Initializable {
         System.out.println("You clicked me");
         label.setText("Hello world");
     }
-
+    ObservableList<User> data;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         TreeTableColumn column1 = new TreeTableColumn<>("Фамилия");
@@ -45,5 +51,39 @@ public class Controller implements Initializable {
         tableview.getColumns().addAll(column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11);
         buttonOne.setText("                  Добавить пациента");
         label.setText("");
+
+        data = FXCollections.observableArrayList(
+                new User("name1", "name2",
+                        "name3","name4",
+                        "name5","name6",
+                        "name7","name8",
+                        "name9"),
+                new User("name1", "name2",
+                        "name3","name4",
+                        "name5","name6",
+                        "name7","name8",
+                        "name9"),
+                new User("name1", "name2",
+                        "name3","name4",
+                        "name5","name6",
+                        "name7","name8",
+                        "name9")
+        );
+
+        column1.setCellValueFactory(
+                new TreeItemPropertyValueFactory<User,String>("name1")
+        );
+        column2.setCellValueFactory(
+                new TreeItemPropertyValueFactory<User,String>("name2")
+        );
+        column3.setCellValueFactory(
+                new TreeItemPropertyValueFactory<User,String>("name3")
+        );
+        TreeItem<User> root = new RecursiveTreeItem<>(data, RecursiveTreeObject::getChildren);
+
+        tableview.setRoot(root);
+
+        tableview.setShowRoot(false);
+
     }
 }
